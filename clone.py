da2c2b90ce54d1d0a426786f7bda5c6ddabb31a3
Steven! Ragnarök <steven@nuclearsandwich.com>
2020-01-30 20:41:19 -0800

    Catch errors during bloom and keep running.
    Also cleans up imports.

--- clone.py
+++ clone.py
@@ -1,14 +1,14 @@
 import argparse
 import copy
-import io
 import os
 import os.path
-import shutil
 import subprocess
 import tempfile
+
+import github
 import yaml
 
-from rosdistro import DistributionFile, get_distribution_cache, get_distribution_file, get_index, get_index_url
+from rosdistro import DistributionFile, get_distribution_cache, get_distribution_file, get_index
 from rosdistro.writer import yaml_from_distribution_file
 
 parser = argparse.ArgumentParser(
@@ -37,8 +37,8 @@ if len(index_yaml['distributions'][args.source]['distribution']) != 1 or \
             raise RuntimeError('Both source and destination distributions must have a single distribution file.')
 
 
-source_distfile_at_ref = subprocess.run(['git', 'show', '{}:{}/distribution.yaml'.format(args.source_ref, args.source)], stdout=subprocess.PIPE, universal_newlines=True)
-source_distfile_data = yaml.safe_load(source_distfile_at_ref.stdout)
+source_distfile_at_ref = subprocess.check_output(['git', 'show', '{}:{}/distribution.yaml'.format(args.source_ref, args.source)], universal_newlines=True)
+source_distfile_data = yaml.safe_load(source_distfile_at_ref)
 source_distribution = DistributionFile(args.source, source_distfile_data)
 
 destpath = index_yaml['distributions'][args.dest]['distribution'][0]
@@ -46,6 +46,8 @@ dest_distribution = get_distribution_file(index, args.dest)
 new_repositories = []
 repositories_to_retry = []
 for repo_name, repo_data in sorted(source_distribution.repositories.items()):
+    if repo_name not in ['fmi_adapter_ros2']:
+        continue
     if repo_name not in dest_distribution.repositories:
         new_repositories.append(repo_name)
         dest_distribution.repositories[repo_name] = copy.deepcopy(repo_data)
@@ -63,54 +65,59 @@ with open(destpath, 'w') as f:
     f.write(yaml_from_distribution_file(dest_distribution))
 
 repositories_bloomed = []
-packages_bloomed = []
+repositories_with_errors = []
 
 workdir = tempfile.mkdtemp()
 os.chdir(workdir)
 os.environ['ROSDISTRO_INDEX_URL'] = rosdistro_index_url
 
 for repo_name in sorted(new_repositories + repositories_to_retry):
-    release_spec = dest_distribution.repositories[repo_name].release_repository
-    print("Adding repo:", repo_name)
-    if release_spec.type != 'git':
-        raise RuntimeError("This script can only handle git repositories.")
-    remote_url = release_spec.url
-    release_repo = remote_url.split('/')[-1][:-4]
-    subprocess.call(['git', 'clone', remote_url])
-    os.chdir(release_repo)
-    tracks = yaml.load(open('tracks.yaml', 'r'))
-    new_release_repo_url = 'git@github.com:nuclearsandwich-ros/{}.git'.format(release_repo)
-    dest_track = copy.deepcopy(tracks['tracks'][args.source])
-    dest_track['ros_distro'] = args.dest
-    tracks['tracks'][args.dest] = dest_track
-    with open('tracks.yaml', 'w') as f:
-        yaml.safe_dump(tracks, f, default_flow_style=False)
-    subprocess.run(['hub', 'create', 'nuclearsandwich-ros/{}'.format(release_repo)])
-    subprocess.run(['git', 'remote', 'rename', 'origin', 'oldorigin'])
-    subprocess.run(['git', 'remote', 'set-url', '--push', 'oldorigin', 'no_push'])
-    subprocess.run(['git', 'remote', 'add', 'origin', new_release_repo_url])
-    subprocess.run(['git', 'add', 'tracks.yaml'])
-    subprocess.run(['git', 'commit', '-m', 'Copy {} track to {} with clone.py.'.format(args.source,args.dest)])
-    ls_remote = subprocess.run(['git', 'ls-remote', '--heads', 'oldorigin', '*{}*'.format(args.source)], stdout=subprocess.PIPE, universal_newlines=True)
-    for line in ls_remote.stdout.split('\n'):
-        if line == '':
-            continue
-        obj, ref = line.split('\t')
-        ref = ref[11:] # strip 'refs/heads/'
-        newref = ref.replace(args.source, args.dest)
-        subprocess.run(['git', 'branch', newref, obj])
-    # Bloom will not run with multiple remotes.
-    subprocess.run(['git', 'remote', 'remove', 'oldorigin'])
-    subprocess.run(['git', 'bloom-release', '--unsafe', args.dest], env=os.environ)
-    subprocess.run(['git', 'push', 'origin', '--all', '--force'])
-    subprocess.run(['git', 'push', 'origin', '--tags', '--force'])
-    new_release_track_inc = str(int(tracks['tracks'][args.dest]['release_inc']) + 1)
-    release_spec.url = new_release_repo_url
+    try:
+        release_spec = dest_distribution.repositories[repo_name].release_repository
+        print("Adding repo:", repo_name)
+        if release_spec.type != 'git':
+            raise ValueError("This script can only handle git repositories.")
+        if repo_name in ['fmi_adapter_ros2', 'behaviortree_cpp_v3', 'system_modes', 'urdfdom_headers']:
+            raise ValueError("Bloom interactivity required")
+        remote_url = release_spec.url
+        release_repo = remote_url.split('/')[-1][:-4]
+        subprocess.call(['git', 'clone', remote_url])
+        os.chdir(release_repo)
+        tracks = yaml.load(open('tracks.yaml', 'r'))
+        new_release_repo_url = 'git@github.com:nuclearsandwich-ros/{}.git'.format(release_repo)
+        dest_track = copy.deepcopy(tracks['tracks'][args.source])
+        dest_track['ros_distro'] = args.dest
+        tracks['tracks'][args.dest] = dest_track
+        with open('tracks.yaml', 'w') as f:
+            yaml.safe_dump(tracks, f, default_flow_style=False)
+        subprocess.check_call(['hub', 'create', 'nuclearsandwich-ros/{}'.format(release_repo)])
+        subprocess.check_call(['git', 'remote', 'rename', 'origin', 'oldorigin'])
+        subprocess.check_call(['git', 'remote', 'set-url', '--push', 'oldorigin', 'no_push'])
+        subprocess.check_call(['git', 'remote', 'add', 'origin', new_release_repo_url])
+        subprocess.check_call(['git', 'add', 'tracks.yaml'])
+        subprocess.check_call(['git', 'commit', '-m', 'Copy {} track to {} with clone.py.'.format(args.source, args.dest)])
+        ls_remote = subprocess.check_output(['git', 'ls-remote', '--heads', 'oldorigin', '*{}*'.format(args.source)], universal_newlines=True)
+        for line in ls_remote.split('\n'):
+            if line == '':
+                continue
+            obj, ref = line.split('\t')
+            ref = ref[11:] # strip 'refs/heads/'
+            newref = ref.replace(args.source, args.dest)
+            subprocess.check_call(['git', 'branch', newref, obj])
+        # Bloom will not run with multiple remotes.
+        subprocess.check_call(['git', 'remote', 'remove', 'oldorigin'])
+        subprocess.check_call(['git', 'bloom-release', '--unsafe', args.dest], env=os.environ)
+        subprocess.check_call(['git', 'push', 'origin', '--all', '--force'])
+        subprocess.check_call(['git', 'push', 'origin', '--tags', '--force'])
+        new_release_track_inc = str(int(tracks['tracks'][args.dest]['release_inc']) + 1)
+        release_spec.url = new_release_repo_url
 
-    ver, _inc = release_spec.version.split('-')
-    release_spec.version = '-'.join([ver, new_release_track_inc])
-    repositories_bloomed.append(repo_name)
-    os.chdir('..')
+        ver, _inc = release_spec.version.split('-')
+        release_spec.version = '-'.join([ver, new_release_track_inc])
+        repositories_bloomed.append(repo_name)
+    except (subprocess.CalledProcessError, ValueError) as e:
+        repositories_with_errors.append((repo_name, e))
+    os.chdir(workdir)
 
 os.chdir(rosdistro_dir)
 
@@ -123,3 +130,4 @@ with open(destpath, 'w') as f:
     f.write(yaml_from_distribution_file(dest_distribution))
 
 print('done')
+print('Had {} repositories with errors:'.format(len(repositories_with_errors)), repositories_with_errors)
